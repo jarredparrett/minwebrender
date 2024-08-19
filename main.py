0453721948b7346eb9565003168e7d13b1b98aea
JarredParrettDickinson <parrettj@dickinson.edu>
2024-08-18 22:10:59 -0400

    Updated port

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes import init_routes
-
+import os
 app = Flask(__name__)
 
 init_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
